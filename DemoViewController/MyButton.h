3d46923d642dfea69f42a1a78fef5e410b6ecba5
冯立海 <335418265@qq.com>
2020-09-22 15:51:47 +0800

    no message

---
+++ DemoViewController/MyButton.h
@@ -0,0 +1,17 @@
+//
+//  MyButton.h
+//  DemoViewController
+//
+//  Created by 冯立海 on 2020/9/20.
+//  Copyright © 2020 banmalu.top. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyButton : UIButton
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DemoViewController/MyButton.m
@@ -0,0 +1,21 @@
+//
+//  MyButton.m
+//  DemoViewController
+//
+//  Created by 冯立海 on 2020/9/20.
+//  Copyright © 2020 banmalu.top. All rights reserved.
+//
+
+#import "MyButton.h"
+
+@implementation MyButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
