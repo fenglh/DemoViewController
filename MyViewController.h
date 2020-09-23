afac51f63a34f5dbf7152b88feb93537da0337ca
冯立海 <335418265@qq.com>
2020-09-23 09:01:26 +0800

    no message

---
+++ MyViewController.h
@@ -0,0 +1,17 @@
+//
+//  MyViewController.h
+//  DemoViewController
+//
+//  Created by 冯立海 on 2020/9/22.
+//  Copyright © 2020 banmalu.top. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyViewController.m
@@ -0,0 +1,37 @@
+//
+//  MyViewController.m
+//  DemoViewController
+//
+//  Created by 冯立海 on 2020/9/22.
+//  Copyright © 2020 banmalu.top. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface MyViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *myButton;
+
+@end
+
+@implementation MyViewController
+- (IBAction)touchUpInside:(id)sender {
+}
+- (IBAction)touchDown:(id)sender {
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
