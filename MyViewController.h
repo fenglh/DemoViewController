ec3873f154f0018e0909a9894bd7630a47c575cf
冯立海 <335418265@qq.com>
2020-09-23 12:01:41 +0800

    no message

--- MyViewController.h
+++ MyViewController.h
@@ -11,6 +11,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface MyViewController : UIViewController
+- (IBAction)dragInside:(id)sender;
 
 @end
 
--- MyViewController.m
+++ MyViewController.m
@@ -34,4 +34,6 @@
 }
 */
 
+- (IBAction)dragInside:(id)sender {
+}
 @end
